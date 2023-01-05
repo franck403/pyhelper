2b6665297f7e3ee723d21355a8bedf50985be95e
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-05 14:44:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import code
 
+# var call
 cmd = code.utils()
 ai = code.ai()
 setting = ai.setting()
 
+# set up the setting
 setting.log(False)
 setting.optimise(True)
 setting.bypass(True,5000)
 
+# Start the ai
 ai.prepare()
 ai.info()
 ai.start(568, 400, 600,5000)
