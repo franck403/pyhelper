76e4f351c850c1dd510fcfb0c50cdcdd5a2e2922
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 20:30:56 +0000

    fix other bug

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ ai = code_1.ai()
 setting = ai.setting()
 
 # code to test
-cusbar = code_1.bar.custombar.fonction()
-cusbar.prepare("Teste",150)
-cusbar.codeer("wait","import time\ntime.sleep(0.1)")
-cusbar.codeer("next",'''import code_1\ndic = code_1.bar.custombar.fonction()\nprint("Computing |" + dic.repeat('#',dic.acrange()) + dic.repeat('-',dic.rerange()) + '|' + str(int(dic.acrange())) + "/" + str(int(dic.rerange())))''')
-cusbar.codeer("loop","import code_1\nimport time\ndic = code_1.bar.custombar.fonction()\ncusbar = code_1.bar.custombar.fonction()\nloop_range = dic.getsrange()\nfor i in range(loop_range):\n   cusbar.run('next')\n   time.sleep(0.1)\n   if dic.acrange() >= loop_range:\n        break")
-cusbar.run("loop")
-quit()
+#cusbar = code_1.bar.custombar.fonction()
+#cusbar.prepare("Teste",150)
+#cusbar.codeer("wait","import time\ntime.sleep(0.1)")
+#cusbar.codeer("next",'''import code_1\ndic = code_1.bar.custombar.fonction()\nprint("Computing |" + dic.repeat('#',dic.acrange()) + dic.repeat('-',dic.rerange()) + '|' + str(int(dic.acrange())) + "/" + str(int(dic.rerange())))''')
+#cusbar.codeer("loop","import code_1\nimport time\ndic = code_1.bar.custombar.fonction()\ncusbar = code_1.bar.custombar.fonction()\nloop_range = dic.getsrange()\nfor i in range(loop_range):\n   cusbar.run('next')\n   time.sleep(0.1)\n   if dic.acrange() >= loop_range:\n        break")
+#cusbar.run("loop")
 
 # set up the setting
 setting.log(False)
