e07228a418db02cab1e2d209ec0e177cf5171d92
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 12:37:42 +0000

    fix bug

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ cmd = code_1.utils()
 ai = code_1.ai()
 setting = ai.setting()
 
-# code to test
-cusbar = code_1.bar.custombar.fonction()
-getco = code_1.bar.custombar()
-getco.getcode("https://raw.githubusercontent.com/franck403/pyhelper/54e499b61754e7a1e2a78b4aafab2e4d2bffb23a/bar.py")
-cusbar.define("start",10,23)
-cusbar.run("start")
-quit()
-
 # set up the setting
 setting.log(False)
 setting.optimise(True)
