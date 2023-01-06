a133a957089f9ee8de6e6a20f26e0713e762a252
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 19:55:54 +0000

    fix bug

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ setting = ai.setting()
 
 # code to test
 cusbar = code_1.bar.custombar.fonction()
-cusbar.prepare("Teste",10)
+cusbar.prepare("Teste",150)
 cusbar.codeer("wait","import time\ntime.sleep(0.1)")
-cusbar.codeer("start","print('hi')")
-cusbar.codeer("next","import code_1\ndic = code_1.bar.custombar.fonction()\nprint('|' + dic.repeat('#',dic.acrange()) + dic.repeat('-',dic.rerange()) + '|')\n")
-cusbar.codeer("loop","import code_1\ndic = code_1.bar.custombar.fonction()\ncusbar = code_1.bar.custombar.fonction()\nloop_range = dic.getsrange()\nfor i in range(loop_range):\n   cusbar.run('next')\n    cusbar.run('wait')")
-cusbar.run("start")
+cusbar.codeer("next","import code_1\ndic = code_1.bar.custombar.fonction()\nprint('Computing |' + dic.repeat('#',dic.acrange()) + dic.repeat('-',dic.rerange()) + '|')\n")
+cusbar.codeer("loop","import code_1\nimport time\ndic = code_1.bar.custombar.fonction()\ncusbar = code_1.bar.custombar.fonction()\nloop_range = dic.getsrange()\nfor i in range(loop_range):\n   cusbar.run('next')\n   time.sleep(0.1)")
 cusbar.run("loop")
 quit()
 
