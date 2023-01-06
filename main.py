9ffcf6ddab7c10a0046bf74c68c23fca5c203741
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 14:17:48 +0000

    fix bug

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ setting = ai.setting()
 # code to test
 cusbar = code_1.bar.custombar.fonction()
 cusbar.prepare("Teste",10)
-cusbar.codeer("advence","dictinary = {'acutal_range':'0'}")
+cusbar.codeer("wait","import time\ntime.sleep(0.1)")
 cusbar.codeer("start","print('hi')")
 cusbar.codeer("next","import code_1\ndic = code_1.bar.custombar.fonction()\nprint('|' + dic.repeat('#',dic.acrange()) + dic.repeat('-',dic.rerange()) + '|')\n")
 cusbar.run("start")
