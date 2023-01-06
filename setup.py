df9676be40ca07f1e7e22a7c2a170f85d2b1eab9
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 20:51:45 +0000

    fix bug

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='Distutils',
       version='0.1',
       description='For help with progress bar ',
+      long_description='For help with progress bar. You can print in colors to :)',
       author='Geoloup Team',
       author_email='franckiebbb@gmail.com',
       packages=['pyHelper'],
