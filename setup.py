6a2c31aa4cb5e7c0a3eda3e3ac2f96afe43f84b6
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-06 20:45:10 +0000

    driugfijg

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='For help with progress bar ',
+      author='Geoloup Team',
+      author_email='franckiebbb@gmail.com',
+      packages=['pyHelper'],
+     )
