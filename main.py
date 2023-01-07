a5efa8f10479202431246999bef45c4444be484a
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-07 22:27:25 +0000

    other try

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ setting = ai.setting()
 # set up the setting
 setting.log(False)
 setting.optimise(True)
-setting.bypass(True,5000)
+setting.bypass(False)
 
 # Start the ai
 ai.prepare()
 ai.info()
-ai.start(568, 400, 600,5000)
+ai.start(568, 400, 600,1000)
